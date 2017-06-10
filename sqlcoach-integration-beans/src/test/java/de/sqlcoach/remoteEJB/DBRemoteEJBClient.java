180493f5f46019cbb1d33fa435e79dcc166a7d55
SQLCoach <SQLCoach@WIN-SQ6NF2ML92F.localdomain>
2017-06-10 22:48:18 +0200

    remove redundant license entries

--- sqlcoach-integration-beans/src/test/java/de/sqlcoach/remoteEJB/DBRemoteEJBClient.java
+++ sqlcoach-integration-beans/src/test/java/de/sqlcoach/remoteEJB/DBRemoteEJBClient.java
@@ -14,22 +14,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
-/*******************************************************************************
- * This file is part of SQLCoach.
- *
- * SQLCoach is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SQLCoach is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
- *******************************************************************************/
 package de.sqlcoach.remoteEJB;
 
 import java.util.Hashtable;
